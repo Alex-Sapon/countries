5a0f57d416f5fca9b7e883bb823aac016b8a2011
Aleksandr_Saponchik <alexsapon@gmail.com>
2023-03-22 21:01:23 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: 'https://alex-sapon.github.io/countries/',
   resolve: {
     alias: {
       components: '/src/components',
